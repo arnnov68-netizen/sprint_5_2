55e10ec68e78c8c5552651590109aa3c700fafdd
arnnov68-netizen <arnnov68@gmail.com>
2026-07-21 21:54:47 +0300

    Update locators.py

--- locators.py
+++ locators.py
@@ -14,12 +14,9 @@ class MainPageLocators:
     # Кнопки
     PLACE_ORDER_BUTTON = (By.XPATH, "//button[contains(text(), 'Оформить заказ')]")
     LOGIN_BUTTON_MAIN = (By.XPATH, "//button[contains(text(), 'Войти в аккаунт')]")
+    LOGIN_BUTTON_MAIN_ALT = (By.XPATH, "//button[contains(@class, 'button') and contains(text(), 'Войти')]")
     PERSONAL_ACCOUNT_BUTTON = (By.XPATH, "//a[contains(@href, '/account')]")
-
-    # Секции
-    BUNS_SECTION = (By.XPATH, "//h2[contains(text(), 'Булки')]")
-    SAUCES_SECTION = (By.XPATH, "//h2[contains(text(), 'Соусы')]")
-    FILLINGS_SECTION = (By.XPATH, "//h2[contains(text(), 'Начинки')]")
+    PERSONAL_ACCOUNT_BUTTON_ALT = (By.XPATH, "//p[contains(text(), 'Личный кабинет')]/parent::a")
 
 
 class LoginPageLocators:
@@ -34,38 +31,31 @@ class LoginPageLocators:
 class RegisterPageLocators:
     """Локаторы для страницы регистрации"""
     REGISTER_BUTTON = (By.XPATH, "//button[contains(text(), 'Зарегистрироваться')]")
-    NAME_INPUT = (By.CSS_SELECTOR, "input[name='name']")
-    EMAIL_INPUT = (By.CSS_SELECTOR, "input[name='email']")
-    PASSWORD_INPUT = (By.CSS_SELECTOR, "input[name='password']")
+    NAME_INPUT = (By.XPATH, "//input[@name='name']")
+    EMAIL_INPUT = (By.XPATH, "//input[@name='email']")
+    PASSWORD_INPUT = (By.XPATH, "//input[@name='password']")
     LOGIN_LINK = (By.XPATH, "//a[contains(text(), 'Войти')]")
-    ERROR_MESSAGE = (By.XPATH, "//p[contains(@class, 'input__error')]")
 
 
 class ForgotPasswordPageLocators:
     """Локаторы для страницы восстановления пароля"""
     RECOVER_BUTTON = (By.XPATH, "//button[contains(text(), 'Восстановить')]")
-    EMAIL_INPUT = (By.CSS_SELECTOR, "input[name='email']")
+    EMAIL_INPUT = (By.XPATH, "//input[@name='email']")
     LOGIN_LINK = (By.XPATH, "//a[contains(text(), 'Войти')]")
 
 
 class ProfilePageLocators:
-    """Локаторы для страницы личного кабинета (профиля)"""
-    # Основные элементы
-    PROFILE_NAME = (By.XPATH, "//p[contains(@class, 'text') and contains(text(), 'Имя')]/following-sibling::p")
-    PROFILE_EMAIL = (By.XPATH, "//p[contains(@class, 'text') and contains(text(), 'Email')]/following-sibling::p")
-
-    # Кнопки и ссылки
+    """Локаторы для страницы личного кабинета"""
     LOGOUT_BUTTON = (By.XPATH, "//button[contains(text(), 'Выход')]")
-    CONSTRUCTOR_BUTTON = (By.XPATH, "//a[contains(text(), 'Конструктор')]")
+
+    # Исправленные локаторы для навигации
+    CONSTRUCTOR_BUTTON = (By.XPATH, "//a[contains(@href, '/') and contains(text(), 'Конструктор')]")
+    CONSTRUCTOR_BUTTON_ALT = (By.XPATH,
+                              "//a[contains(@class, 'AppHeader_header__link')][.//p[contains(text(), 'Конструктор')]]")
+
     LOGO = (By.XPATH, "//a[contains(@class, 'AppHeader_header__logo')]")
+    LOGO_ALT = (By.XPATH,
+                "//a[contains(@href, '/')]//*[local-name()='svg' and contains(@class, 'AppHeader_header__logo')]/parent::a")
+
     ORDER_HISTORY_LINK = (By.XPATH, "//a[contains(text(), 'История заказов')]")
     PROFILE_LINK = (By.XPATH, "//a[contains(text(), 'Профиль')]")
-
-    # Поля для редактирования профиля
-    NAME_INPUT = (By.XPATH, "//input[@name='name']")
-    EMAIL_INPUT = (By.XPATH, "//input[@name='email']")
-    PASSWORD_INPUT = (By.XPATH, "//input[@name='password']")
-
-    # Кнопки действий
-    SAVE_BUTTON = (By.XPATH, "//button[contains(text(), 'Сохранить')]")
-    CANCEL_BUTTON = (By.XPATH, "//button[contains(text(), 'Отмена')]")
